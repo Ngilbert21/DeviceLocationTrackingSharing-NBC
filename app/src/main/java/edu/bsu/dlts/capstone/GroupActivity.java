4db5fd16cbeb644328f1d1298727418d9db9ff34
Ngilbert21 <peachbook121>
2019-02-25 19:17:53 -0500

    Initial commit

---
+++ app/src/main/java/edu/bsu/dlts/capstone/GroupActivity.java
@@ -0,0 +1,44 @@
+package edu.bsu.dlts.capstone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        configureAddUserButton();
+    }
+
+    private void configureAddUserButton(){
+        Button addUser = (Button) findViewById(R.id.button12);
+        addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent7 = new Intent(GroupActivity.this, InvitationActivity.class);
+                startActivity(intent7);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/edu/bsu/dlts/capstone/MainMenu.java
@@ -0,0 +1,56 @@
+package edu.bsu.dlts.capstone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        configureTourButton();
+        configureBrowseToursButton();
+    }
+
+    private void configureTourButton(){
+        Button startTour = (Button) findViewById(R.id.button4);
+        startTour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainMenu.this, TourActivity.class);
+                startActivity(intent2);
+            }
+        });
+    }
+
+    private void configureBrowseToursButton(){
+        Button prevTour = (Button) findViewById(R.id.button5);
+        prevTour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MainMenu.this, PreviousTours.class);
+                startActivity(intent3);
+            }
+        });
+    }
+
+}
