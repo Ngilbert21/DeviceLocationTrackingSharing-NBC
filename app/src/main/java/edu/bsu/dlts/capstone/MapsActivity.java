2dc0ae3250dee5ec28ade11a4c32b5ba32c9a1fd
Bradley Strayer <bstrayer0@gmail.com>
2019-02-26 07:34:22 -0500

    Map works

--- app/src/main/java/edu/bsu/dlts/capstone/MapsActivity.java
+++ app/src/main/java/edu/bsu/dlts/capstone/MapsActivity.java
@@ -1,13 +1,19 @@
 package edu.bsu.dlts.capstone;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,11 +22,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Calendar;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     private Boolean mLocationPermissionGranted = false;
+
+    private Location currentLocation;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
@@ -77,13 +87,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener(mMap);
+
+        if (mLocationPermissionGranted && locationManager != null){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        }
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+}
+
+class MyLocationListener implements LocationListener {
+    private GoogleMap googleMap;
+
+    MyLocationListener(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+//            String locationString = String.format("%.4f %.4f\n", location.getLatitude(), location.getLongitude());
+//            txtLocation.setText(txtLocation.getText() + locationString);
+
+            LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+            googleMap.addMarker(new MarkerOptions().position(current).title("Location at " + Calendar.getInstance().getTime().toString()));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+    @Override
+    public void onProviderEnabled(String s) {}
+
+    @Override
+    public void onProviderDisabled(String s) {}
 }
