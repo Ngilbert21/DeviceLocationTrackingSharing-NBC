54e6554ec6de63314a2dd43a3a18e54c29de3e8a
Nick Gilbert <nagilbert@bsu.edu>
2019-03-12 22:03:21 -0400

    Added missing things.

---
+++ app/src/main/java/edu/bsu/dlts/capstone/NewGroupActivity.java
@@ -0,0 +1,171 @@
+package edu.bsu.dlts.capstone;
+
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NewGroupActivity extends AppCompatActivity {
+
+    private DatabaseReference RootRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
+    private ListView list_view;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_groups = new ArrayList<>();
+    private DatabaseReference GroupRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+
+        //Set toolbar and logout / Join Menu
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Group Area");
+        setSupportActionBar(toolbar);
+
+        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
+        
+        InitializeFields();
+
+        RetrieveAndDisplayGroups();
+    }
+
+    private void RetrieveAndDisplayGroups() {
+        GroupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<>();
+
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while (iterator.hasNext()){
+
+                    set.add(((DataSnapshot)iterator.next()).getKey());
+
+                }
+                list_of_groups.clear();
+                list_of_groups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        list_view = (ListView)findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(NewGroupActivity.this, android.R.layout.simple_list_item_1, list_of_groups);
+        list_view.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ddcornermenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_invite:
+
+                break;
+            case R.id.action_creategroup:
+                RequestNewGroup();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void RequestNewGroup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(NewGroupActivity.this, R.style.AlertDialog);
+        builder.setTitle("Enter Group Name : ");
+
+        final EditText groupNameField = new EditText(NewGroupActivity.this);
+        groupNameField.setHint("e.g. BSU Field Trip");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+                String groupName = groupNameField.getText().toString();
+
+                if(TextUtils.isEmpty(groupName)){
+
+                    Toast.makeText(NewGroupActivity.this,"Please Write Group Name...", Toast.LENGTH_SHORT);
+                }else {
+                    
+                    CreateNewGroup(groupName);
+
+                }
+
+            }
+        }
+
+
+
+        );
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+                dialogInterface.cancel();
+
+            }
+        });
+        builder.show();
+    }
+
+    private void CreateNewGroup(final String groupName) {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(NewGroupActivity.this, groupName+" group is created Successfully....", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+}
